f79d666219c1c02c9dcd6bf5029ff461c16b6ada
Mohit <mohit2002ss@gmail.com>
2021-06-09 13:18:06 +0530

    Reformatted code

--- src/main/kotlin/com/looker/droidify/database/QueryBuilder.kt
+++ src/main/kotlin/com/looker/droidify/database/QueryBuilder.kt
@@ -8,40 +8,43 @@ import com.looker.droidify.utility.extension.android.asSequence
 import com.looker.droidify.utility.extension.text.debug
 
 class QueryBuilder {
-  companion object {
-    fun trimQuery(query: String): String {
-      return query.lines().map { it.trim() }.filter { it.isNotEmpty() }.joinToString(separator = " ")
+    companion object {
+        fun trimQuery(query: String): String {
+            return query.lines().map { it.trim() }.filter { it.isNotEmpty() }
+                .joinToString(separator = " ")
+        }
     }
-  }
 
-  private val builder = StringBuilder()
-  private val arguments = mutableListOf<String>()
+    private val builder = StringBuilder()
+    private val arguments = mutableListOf<String>()
 
-  operator fun plusAssign(query: String) {
-    if (builder.isNotEmpty()) {
-      builder.append(" ")
+    operator fun plusAssign(query: String) {
+        if (builder.isNotEmpty()) {
+            builder.append(" ")
+        }
+        builder.append(trimQuery(query))
     }
-    builder.append(trimQuery(query))
-  }
 
-  operator fun remAssign(argument: String) {
-    this.arguments += argument
-  }
-
-  operator fun remAssign(arguments: List<String>) {
-    this.arguments += arguments
-  }
-
-  fun query(db: SQLiteDatabase, signal: CancellationSignal?): Cursor {
-    val query = builder.toString()
-    val arguments = arguments.toTypedArray()
-    if (BuildConfig.DEBUG) {
-      synchronized(QueryBuilder::class.java) {
-        debug(query)
-        db.rawQuery("EXPLAIN QUERY PLAN $query", arguments).use { it.asSequence()
-          .forEach { debug(":: ${it.getString(it.getColumnIndex("detail"))}") } }
-      }
+    operator fun remAssign(argument: String) {
+        this.arguments += argument
     }
-    return db.rawQuery(query, arguments, signal)
-  }
+
+    operator fun remAssign(arguments: List<String>) {
+        this.arguments += arguments
+    }
+
+    fun query(db: SQLiteDatabase, signal: CancellationSignal?): Cursor {
+        val query = builder.toString()
+        val arguments = arguments.toTypedArray()
+        if (BuildConfig.DEBUG) {
+            synchronized(QueryBuilder::class.java) {
+                debug(query)
+                db.rawQuery("EXPLAIN QUERY PLAN $query", arguments).use {
+                    it.asSequence()
+                        .forEach { debug(":: ${it.getString(it.getColumnIndex("detail"))}") }
+                }
+            }
+        }
+        return db.rawQuery(query, arguments, signal)
+    }
 }
