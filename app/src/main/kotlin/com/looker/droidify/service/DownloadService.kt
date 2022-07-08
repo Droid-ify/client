eb358f933fae5cc344534a80cacea4e7167cf6ce
Iamlooker <mohit2002ss@gmail.com>
2022-07-08 14:55:11 +0530

    Fix Android 12 notification trampoline restrictions

--- app/src/main/kotlin/com/looker/droidify/service/DownloadService.kt
+++ app/src/main/kotlin/com/looker/droidify/service/DownloadService.kt
@@ -9,6 +9,7 @@ import android.content.Intent
 import android.net.Uri
 import android.view.ContextThemeWrapper
 import androidx.core.app.NotificationCompat
+import androidx.core.app.TaskStackBuilder
 import com.looker.droidify.BuildConfig
 import com.looker.droidify.Common
 import com.looker.droidify.MainActivity
@@ -206,6 +207,18 @@ class DownloadService : ConnectionService<DownloadService.Binder>() {
 	}
 
 	private fun showNotificationError(task: Task, errorType: ErrorType) {
+		val intent = Intent(this, MainActivity::class.java)
+			.setAction(Intent.ACTION_VIEW)
+			.setData(Uri.parse("package:${task.packageName}"))
+			.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+		val resultPendingIntent: PendingIntent? = TaskStackBuilder.create(this).run {
+			addNextIntentWithParentStack(intent)
+			getPendingIntent(
+				0,
+				if (Android.sdk(31)) PendingIntent.FLAG_UPDATE_CURRENT or PendingIntent.FLAG_MUTABLE
+				else PendingIntent.FLAG_UPDATE_CURRENT
+			)
+		}
 		notificationManager.notify(task.notificationTag,
 			Common.NOTIFICATION_ID_DOWNLOADING,
 			NotificationCompat
@@ -216,18 +229,7 @@ class DownloadService : ConnectionService<DownloadService.Binder>() {
 					ContextThemeWrapper(this, R.style.Theme_Main_Light)
 						.getColorFromAttr(R.attr.colorPrimary).defaultColor
 				)
-				.setContentIntent(
-					PendingIntent.getBroadcast(
-						this,
-						0,
-						Intent(this, Receiver::class.java)
-							.setAction("$ACTION_OPEN.${task.packageName}"),
-						if (Android.sdk(23))
-							PendingIntent.FLAG_UPDATE_CURRENT or PendingIntent.FLAG_IMMUTABLE
-						else
-							PendingIntent.FLAG_UPDATE_CURRENT
-					)
-				)
+				.setContentIntent(resultPendingIntent)
 				.apply {
 					when (errorType) {
 						is ErrorType.Network -> {
@@ -273,6 +275,19 @@ class DownloadService : ConnectionService<DownloadService.Binder>() {
 	}
 
 	private fun showNotificationInstall(task: Task) {
+		val intent = Intent(this, MainActivity::class.java)
+			.setAction(MainActivity.ACTION_INSTALL)
+			.setData(Uri.parse("package:$task.packageName"))
+			.putExtra(MainActivity.EXTRA_CACHE_FILE_NAME, task.release.cacheFileName)
+			.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+		val resultPendingIntent: PendingIntent? = TaskStackBuilder.create(this).run {
+			addNextIntentWithParentStack(intent)
+			getPendingIntent(
+				0,
+				if (Android.sdk(31)) PendingIntent.FLAG_UPDATE_CURRENT or PendingIntent.FLAG_MUTABLE
+				else PendingIntent.FLAG_UPDATE_CURRENT
+			)
+		}
 		notificationManager.notify(
 			task.notificationTag, Common.NOTIFICATION_ID_DOWNLOADING, NotificationCompat
 				.Builder(this, Common.NOTIFICATION_CHANNEL_DOWNLOADING)
@@ -282,22 +297,12 @@ class DownloadService : ConnectionService<DownloadService.Binder>() {
 					ContextThemeWrapper(this, R.style.Theme_Main_Light)
 						.getColorFromAttr(R.attr.colorPrimary).defaultColor
 				)
-				.setContentIntent(installIntent(task))
+				.setContentIntent(resultPendingIntent)
 				.setContentTitle(getString(R.string.downloaded_FORMAT, task.name))
 				.setContentText(getString(R.string.tap_to_install_DESC))
 				.build()
 		)
 	}
-
-	private fun installIntent(task: Task): PendingIntent = PendingIntent.getBroadcast(
-		this,
-		0,
-		Intent(this, Receiver::class.java)
-			.setAction("$ACTION_INSTALL.${task.packageName}")
-			.putExtra(EXTRA_CACHE_FILE_NAME, task.release.cacheFileName),
-		if (Android.sdk(23)) PendingIntent.FLAG_UPDATE_CURRENT or PendingIntent.FLAG_IMMUTABLE
-		else PendingIntent.FLAG_UPDATE_CURRENT
-	)
 
 	private fun publishSuccess(task: Task) {
 		var consumed = false
