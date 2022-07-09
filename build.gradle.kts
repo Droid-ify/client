8d46b4767fd2ce2dfd37bcfa47902af735d1ad0d
Iamlooker <mohit2002ss@gmail.com>
2022-07-09 15:56:30 +0530

    Convert build.gradle to kotlin dsl

---
+++ build.gradle.kts
@@ -0,0 +1,9 @@
+plugins {
+	id("com.android.application") version "7.2.1" apply false
+	id("com.android.library") version "7.2.1" apply false
+	id("org.jetbrains.kotlin.android") version "1.7.0" apply false
+}
+
+tasks.register("clean", Delete::class) {
+	delete(rootProject.buildDir)
+}
