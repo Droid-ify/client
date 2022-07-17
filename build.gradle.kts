1ed9a2a5806ad7bdd5580abe55f157f91534b639
Iamlooker <mohit2002ss@gmail.com>
2022-07-17 19:35:16 +0530

    Add core-data Module
    This module will handle Index Parsing and other secondary functions

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
 	id("com.android.application") version "7.2.1" apply false
 	id("com.android.library") version "7.2.1" apply false
-	id("org.jetbrains.kotlin.android") version Version.kotlinVersion apply false
+	id("org.jetbrains.kotlin.android") version Version.kotlin apply false
 }
 
 tasks.register("clean", Delete::class) {
--- core-data/build.gradle.kts
+++ core-data/build.gradle.kts
@@ -1,13 +1,12 @@
 plugins {
 	id("com.android.library")
 	id("org.jetbrains.kotlin.android")
-	id("com.google.devtools.ksp") version Version.ksp
 	kotlin("plugin.serialization") version Version.kotlin
 }
 
 android {
 	compileSdk = Android.compileSdk
-	namespace = "com.looker.core_database"
+	namespace = "com.looker.core_data"
 	defaultConfig {
 		minSdk = Android.minSdk
 		targetSdk = Android.compileSdk
@@ -35,9 +34,8 @@ android {
 
 dependencies {
 	implementation(project(Modules.coreModel))
+	implementation(project(Modules.coreDatabase))
 	implementation(Core.core)
-	api(Kotlin.serialization)
-	implementation(Room.roomRuntime)
-	implementation(Room.roomKtx)
-	ksp(Room.roomCompiler)
+	implementation(Coroutines.core)
+	implementation(Coroutines.android)
 }
