ea5d08718f82f4517b5fd61f3579226f3cfa29e6
Iamlooker <mohit2002ss@gmail.com>
2022-07-27 01:22:17 +0530

    Add Settings modules

---
+++ feature-settings/build.gradle.kts
@@ -0,0 +1,40 @@
+plugins {
+	id("com.android.library")
+	id("org.jetbrains.kotlin.android")
+}
+
+android {
+	compileSdk = Android.compileSdk
+	namespace = "com.looker.feature_settings"
+	defaultConfig {
+		minSdk = Android.minSdk
+		targetSdk = Android.compileSdk
+
+		consumerProguardFiles("consumer-rules.pro")
+	}
+
+	buildTypes {
+		release {
+			isMinifyEnabled = true
+			proguardFiles(
+				getDefaultProguardFile("proguard-android-optimize.txt"),
+				"proguard-rules.pro"
+			)
+		}
+	}
+	compileOptions {
+		sourceCompatibility = JavaVersion.VERSION_1_8
+		targetCompatibility = JavaVersion.VERSION_1_8
+	}
+	kotlinOptions {
+		jvmTarget = "1.8"
+	}
+}
+
+dependencies {
+	implementation(project(Modules.coreCommon))
+
+	kotlin("stdlib")
+	implementation(Core.core)
+	implementation(AndroidX.material)
+}
