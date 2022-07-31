e0d6605695af2a37a2992efa372167dc597ab1b4
Iamlooker <mohit2002ss@gmail.com>
2022-08-01 01:10:26 +0530

    Remove unused proguard files

--- core-data/build.gradle.kts
+++ core-data/build.gradle.kts
@@ -17,10 +17,7 @@ android {
 	buildTypes {
 		release {
 			isMinifyEnabled = true
-			proguardFiles(
-				getDefaultProguardFile("proguard-android-optimize.txt"),
-				"proguard-rules.pro"
-			)
+			proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"))
 		}
 	}
 	compileOptions {
--- core-datastore/build.gradle.kts
+++ core-datastore/build.gradle.kts
@@ -18,10 +18,7 @@ android {
 	buildTypes {
 		release {
 			isMinifyEnabled = true
-			proguardFiles(
-				getDefaultProguardFile("proguard-android-optimize.txt"),
-				"proguard-rules.pro"
-			)
+			proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"))
 		}
 	}
 	compileOptions {
