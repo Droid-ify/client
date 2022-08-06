eb3453a3de416810bbdc36d67d8f7bb869568bb1
Iamlooker <mohit2002ss@gmail.com>
2022-08-06 12:36:48 +0530

    Update coroutines versions

--- buildSrc/src/main/kotlin/Libs.kt
+++ buildSrc/src/main/kotlin/Libs.kt
@@ -21,7 +21,7 @@ object Coil {
 }
 
 object Coroutines {
-	private const val coroutinesVersion = "1.6.3"
+	private const val coroutinesVersion = "1.6.4"
 	const val core = "org.jetbrains.kotlinx:kotlinx-coroutines-core:$coroutinesVersion"
 	const val android = "org.jetbrains.kotlinx:kotlinx-coroutines-android:$coroutinesVersion"
 }
