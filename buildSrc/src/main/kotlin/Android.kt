c8c07e078c3ab67ebd633c6f9076ab767681f7e7
Iamlooker <mohit2002ss@gmail.com>
2022-08-30 17:57:20 +0530

    Release 0.4.9

--- buildSrc/src/main/kotlin/Android.kt
+++ buildSrc/src/main/kotlin/Android.kt
@@ -2,6 +2,6 @@ object Android {
 	const val appId = "com.looker.droidify"
 	const val compileSdk = 32
 	const val minSdk = 23
-	const val versionCode = 48
-	const val versionName = "0.4.8"
+	const val versionCode = 49
+	const val versionName = "0.4.9"
 }
