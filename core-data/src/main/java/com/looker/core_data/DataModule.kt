57666d03fcd1a4c973dfd0d23846d08e61638ab8
LooKeR <mohit2002ss@gmail.com>
2022-11-01 00:03:50 +0530

    Fix build issue Unimplemented Hilt interface

--- core-data/src/main/java/com/looker/core_data/DataModule.kt
+++ core-data/src/main/java/com/looker/core_data/DataModule.kt
@@ -4,6 +4,7 @@ import com.looker.core_data.repository.AppRepository
 import com.looker.core_data.repository.RepoRepository
 import com.looker.core_data.repository.impl.AppRepositoryImpl
 import com.looker.core_data.repository.impl.RepoRepositoryImpl
+import dagger.Binds
 import dagger.Module
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
@@ -12,10 +13,12 @@ import dagger.hilt.components.SingletonComponent
 @InstallIn(SingletonComponent::class)
 interface DataModule {
 
+	@Binds
 	fun bindAppRepository(
 		appRepositoryImpl: AppRepositoryImpl
 	): AppRepository
 
+	@Binds
 	fun bindRepoRepository(
 		repoRepositoryImpl: RepoRepositoryImpl
 	): RepoRepository
