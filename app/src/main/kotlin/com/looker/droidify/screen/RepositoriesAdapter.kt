Droid-ify/client

commit 73aa2c1cb4f62abc1fcb6012e28b011ce0e68cfc

Add a floating action button to scroll to the top

app/src/main/kotlin/com/looker/droidify/screen/ScreenshotsAdapter.kt
@@ -88,7 +88,7 @@ class ScreenshotsAdapter(private val onClick: (Product.Screenshot) -> Unit) :
 		viewType: ViewType
 	): RecyclerView.ViewHolder {
 		return ViewHolder(parent.context).apply {
-			itemView.setOnClickListener { onClick(items[absoluteAdapterPosition].screenshot) }
+			itemView.setOnClickListener { onClick(items[adapterPosition].screenshot) }
 		}
 	}
 

app/src/main/kotlin/com/looker/droidify/ui/app_detail/AppDetailAdapter.kt
@@ -966,7 +966,7 @@ class AppDetailAdapter(private val callbacks: Callbacks) :
 			ViewType.SCREENSHOT -> ScreenShotViewHolder(parent.context)
 			ViewType.SWITCH -> SwitchViewHolder(parent.inflate(R.layout.switch_item)).apply {
 				itemView.setOnClickListener {
-					val switchItem = items[absoluteAdapterPosition] as Item.SwitchItem
+					val switchItem = items[adapterPosition] as Item.SwitchItem
 					val productPreference = when (switchItem.switchType) {
 						SwitchType.IGNORE_ALL_UPDATES -> {
 							ProductPreferences[switchItem.packageName].let { it.copy(ignoreUpdates = !it.ignoreUpdates) }
@@ -991,7 +991,7 @@ class AppDetailAdapter(private val callbacks: Callbacks) :
 			}
 			ViewType.SECTION -> SectionViewHolder(parent.inflate(R.layout.section_item)).apply {
 				itemView.setOnClickListener {
-					val position = absoluteAdapterPosition
+					val position = adapterPosition
 					val sectionItem = items[position] as Item.SectionItem
 					if (sectionItem.items.isNotEmpty()) {
 						expanded += sectionItem.expandType
@@ -1017,7 +1017,7 @@ class AppDetailAdapter(private val callbacks: Callbacks) :
 			}
 			ViewType.EXPAND -> ExpandViewHolder(parent.context).apply {
 				itemView.setOnClickListener {
-					val position = absoluteAdapterPosition
+					val position = adapterPosition
 					val expandItem = items[position] as Item.ExpandItem
 					if (expandItem.expandType !in expanded) {
 						expanded += expandItem.expandType
@@ -1052,20 +1052,20 @@ class AppDetailAdapter(private val callbacks: Callbacks) :
 			ViewType.TEXT -> TextViewHolder(parent.context)
 			ViewType.LINK -> LinkViewHolder(parent.inflate(R.layout.link_item)).apply {
 				itemView.setOnClickListener {
-					val linkItem = items[absoluteAdapterPosition] as Item.LinkItem
+					val linkItem = items[adapterPosition] as Item.LinkItem
 					if (linkItem.uri?.let { callbacks.onUriClick(it, false) } != true) {
 						linkItem.displayLink?.let { copyLinkToClipboard(itemView, it) }
 					}
 				}
 				itemView.setOnLongClickListener {
-					val linkItem = items[absoluteAdapterPosition] as Item.LinkItem
+					val linkItem = items[adapterPosition] as Item.LinkItem
 					linkItem.displayLink?.let { copyLinkToClipboard(itemView, it) }
 					true
 				}
 			}
 			ViewType.PERMISSIONS -> PermissionsViewHolder(parent.inflate(R.layout.permissions_item)).apply {
 				itemView.setOnClickListener {
-					val permissionsItem = items[absoluteAdapterPosition] as Item.PermissionsItem
+					val permissionsItem = items[adapterPosition] as Item.PermissionsItem
 					callbacks.onPermissionsClick(
 						permissionsItem.group?.name,
 						permissionsItem.permissions.map { it.name })
@@ -1073,11 +1073,11 @@ class AppDetailAdapter(private val callbacks: Callbacks) :
 			}
 			ViewType.RELEASE -> ReleaseViewHolder(parent.inflate(R.layout.release_item)).apply {
 				itemView.setOnClickListener {
-					val releaseItem = items[absoluteAdapterPosition] as Item.ReleaseItem
+					val releaseItem = items[adapterPosition] as Item.ReleaseItem
 					callbacks.onReleaseClick(releaseItem.release)
 				}
 				itemView.setOnLongClickListener {
-					val releaseItem = items[absoluteAdapterPosition] as Item.ReleaseItem
+					val releaseItem = items[adapterPosition] as Item.ReleaseItem
 					copyLinkToClipboard(
 						itemView,
 						releaseItem.release.getDownloadUrl(releaseItem.repository)

app/src/main/kotlin/com/looker/droidify/ui/app_list/AppListAdapter.kt
@@ -122,7 +122,7 @@ class AppListAdapter(private val onClick: (ProductItem) -> Unit) :
 	): RecyclerView.ViewHolder {
 		return when (viewType) {
 			ViewType.PRODUCT -> ProductViewHolder(parent.inflate(R.layout.product_item)).apply {
-				itemView.setOnClickListener { onClick(getProductItem(absoluteAdapterPosition)) }
+				itemView.setOnClickListener { onClick(getProductItem(adapterPosition)) }
 			}
 			ViewType.LOADING -> LoadingViewHolder(parent.context)
 			ViewType.EMPTY -> EmptyViewHolder(parent.context)

app/src/main/kotlin/com/looker/droidify/ui/tabs_fragment/TabsFragment.kt
@@ -596,7 +596,7 @@ class TabsFragment : ScreenFragment() {
 			viewType: ViewType,
 		): RecyclerView.ViewHolder {
 			return SectionViewHolder(parent.context).apply {
-				itemView.setOnClickListener { onClick(sections()[absoluteAdapterPosition]) }
+				itemView.setOnClickListener { onClick(sections()[adapterPosition]) }
 			}
 		}
 

app/src/main/kotlin/com/looker/droidify/ui/app_list/AppListFragment.kt
@@ -15,6 +15,7 @@ import androidx.recyclerview.widget.RecyclerView
 import com.looker.core_model.ProductItem
 import com.looker.droidify.database.CursorOwner
 import com.looker.droidify.database.Database
+import com.looker.droidify.databinding.RecyclerViewWithFabBinding
 import com.looker.droidify.utility.RxUtils
 import com.looker.droidify.utility.extension.screenActivity
 import dagger.hilt.android.AndroidEntryPoint
@@ -30,6 +31,9 @@ import com.looker.core_common.R.string as stringRes
 class AppListFragment() : Fragment(), CursorOwner.Callback {
 
 	private val viewModel: AppListViewModel by viewModels()
+
+	private var _binding: RecyclerViewWithFabBinding? = null
+	private val binding get() = _binding!!
 
 	companion object {
 		private const val EXTRA_SOURCE = "source"
@@ -50,26 +54,51 @@ class AppListFragment() : Fragment(), CursorOwner.Callback {
 	val source: Source
 		get() = requireArguments().getString(EXTRA_SOURCE)!!.let(Source::valueOf)
 
-	private var recyclerView: RecyclerView? = null
+
+	private lateinit var recyclerViewAdapter: AppListAdapter
 
 	private var repositoriesDisposable: Disposable? = null
+
+	private var shortAnimationDuration: Int = 0
 
 	override fun onCreateView(
 		inflater: LayoutInflater,
 		container: ViewGroup?,
 		savedInstanceState: Bundle?,
 	): View {
-		return RecyclerView(requireContext()).apply {
+		_binding = RecyclerViewWithFabBinding.inflate(inflater, container, false)
+
+		shortAnimationDuration = resources.getInteger(android.R.integer.config_shortAnimTime)
+
+		val recyclerView = binding.recyclerView.apply {
 			id = android.R.id.list
 			layoutManager = LinearLayoutManager(context)
 			isMotionEventSplittingEnabled = false
 			isVerticalScrollBarEnabled = false
 			setHasFixedSize(true)
 			recycledViewPool.setMaxRecycledViews(AppListAdapter.ViewType.PRODUCT.ordinal, 30)
-			val adapter = AppListAdapter { screenActivity.navigateProduct(it.packageName) }
-			this.adapter = adapter
-			recyclerView = this
+			recyclerViewAdapter = AppListAdapter { screenActivity.navigateProduct(it.packageName) }
+			this.adapter = recyclerViewAdapter
 		}
+		val fab = binding.scrollUp
+		fab.setOnClickListener { recyclerView.scrollToPosition(0) }
+		fab.apply {
+			this.alpha = 0f
+			visibility = View.VISIBLE
+		}
+
+		val scrollListener = object : RecyclerView.OnScrollListener() {
+			override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
+				val position = (recyclerView.layoutManager as LinearLayoutManager)
+					.findFirstVisibleItemPosition()
+				fab.animate()
+					.alpha(if (position != 0) 1f else 0f)
+					.setDuration(shortAnimationDuration.toLong())
+					.setListener(null)
+			}
+		}
+		recyclerView.addOnScrollListener(scrollListener)
+		return binding.root
 	}
 
 	override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
@@ -82,21 +111,20 @@ class AppListFragment() : Fragment(), CursorOwner.Callback {
 			.flatMapSingle { RxUtils.querySingle { Database.RepositoryAdapter.getAll(it) } }
 			.map { list -> list.asSequence().map { Pair(it.id, it) }.toMap() }
 			.observeOn(AndroidSchedulers.mainThread())
-			.subscribe { (recyclerView?.adapter as? AppListAdapter)?.repositories = it }
+			.subscribe { recyclerViewAdapter.repositories = it }
 	}
 
 	override fun onDestroyView() {
 		super.onDestroyView()
 
-		recyclerView = null
-
+		_binding = null
 		screenActivity.cursorOwner.detach(this)
 		repositoriesDisposable?.dispose()
 		repositoriesDisposable = null
 	}
 
 	override fun onCursorData(request: CursorOwner.Request, cursor: Cursor?) {
-		(recyclerView?.adapter as? AppListAdapter)?.apply {
+		recyclerViewAdapter.apply {
 			this.cursor = cursor
 			lifecycleScope.launch {
 				repeatOnLifecycle(Lifecycle.State.RESUMED) {

app/src/main/res/layout/recycler_view_with_fab.xml
@@ -1,0 +1,23 @@
+<?xml version="1.0" encoding="utf-8"?>
+<androidx.constraintlayout.widget.ConstraintLayout xmlns:android="http://schemas.android.com/apk/res/android"
+    xmlns:app="http://schemas.android.com/apk/res-auto"
+    xmlns:tools="http://schemas.android.com/tools"
+    android:layout_width="match_parent"
+    android:layout_height="match_parent">
+
+    <androidx.recyclerview.widget.RecyclerView
+        android:id="@+id/recycler_view"
+        android:layout_width="match_parent"
+        android:layout_height="match_parent" />
+
+    <com.google.android.material.floatingactionbutton.FloatingActionButton
+        android:id="@+id/scroll_up"
+        android:layout_width="wrap_content"
+        android:layout_height="wrap_content"
+        android:layout_margin="16dp"
+        android:visibility="gone"
+        android:src="@drawable/arrow_up"
+        app:layout_constraintBottom_toBottomOf="parent"
+        app:layout_constraintEnd_toEndOf="parent"
+        tools:ignore="ContentDescription,SpeakableTextPresentCheck" />
+</androidx.constraintlayout.widget.ConstraintLayout>

core-common/src/main/res/drawable/arrow_up.xml
@@ -1,0 +1,10 @@
+<vector xmlns:android="http://schemas.android.com/apk/res/android"
+    android:width="48dp"
+    android:height="48dp"
+    android:viewportWidth="48"
+    android:viewportHeight="48"
+    android:tint="?attr/colorControlNormal">
+  <path
+      android:fillColor="@android:color/white"
+      android:pathData="M22.5,40V13.7L10.1,26.1L8,24L24,8L40,24L37.9,26.1L25.5,13.7V40Z"/>
+</vector>

commit ead8375a37bab8a0cde2d7f652842ddd34cf2e2f

Better loading of download status This also fixes a crash introduced in 73aa2c1cb4f62abc1fcb6012e28b011ce0e68cfc

app/src/main/kotlin/com/looker/droidify/screen/RepositoriesAdapter.kt
@@ -52,12 +52,12 @@ class RepositoriesAdapter(
 	): RecyclerView.ViewHolder {
 		return ViewHolder(parent.inflate(R.layout.repository_item)).apply {
 			itemView.setOnLongClickListener {
-				onClick(getRepository(adapterPosition))
+				onClick(getRepository(absoluteAdapterPosition))
 				true
 			}
 			itemView.setOnClickListener {
 				isEnabled = !isEnabled
-				onSwitch(getRepository(adapterPosition), isEnabled)
+				onSwitch(getRepository(absoluteAdapterPosition), isEnabled)
 			}
 		}
 	}

app/src/main/kotlin/com/looker/droidify/screen/ScreenshotsAdapter.kt
@@ -88,7 +88,7 @@ class ScreenshotsAdapter(private val onClick: (Product.Screenshot) -> Unit) :
 		viewType: ViewType
 	): RecyclerView.ViewHolder {
 		return ViewHolder(parent.context).apply {
-			itemView.setOnClickListener { onClick(items[adapterPosition].screenshot) }
+			itemView.setOnClickListener { onClick(items[absoluteAdapterPosition].screenshot) }
 		}
 	}
 

app/src/main/kotlin/com/looker/droidify/ui/app_detail/AppDetailAdapter.kt
@@ -103,6 +103,7 @@ class AppDetailAdapter(private val callbacks: Callbacks) :
 	}
 
 	sealed class Status {
+		object Idle : Status()
 		object Pending : Status()
 		object Connecting : Status()
 		data class Downloading(val read: Long, val total: Long?) : Status()
@@ -906,43 +907,28 @@ class AppDetailAdapter(private val callbacks: Callbacks) :
 
 	private var action: Action? = null
 
-	fun setAction(action: Action?) {
-		if (this.action != action) {
-			val translate = this.action == null || action == null ||
-					this.action == Action.CANCEL || action == Action.CANCEL
-			this.action = action
-			val index = items.indexOfFirst { it is Item.AppInfoItem }
+	fun setAction(newAction: Action?) {
+		if (action != newAction) {
+			action = newAction
+			val index = items.indexOf(Item.InstallButtonItem)
 			if (index >= 0) {
-				if (translate) {
-					notifyItemInserted(index + 1)
-					notifyItemChanged(index + 2)
-				} else {
-					notifyItemInserted(index + 1)
-					notifyItemChanged(index + 2, Payload.REFRESH)
-				}
+				notifyItemChanged(index)
 			}
 		}
 	}
 
-	private var status: Status? = null
+	private var status: Status = Status.Idle
 
-	fun setStatus(status: Status?) {
-		val translate = (this.status == null) != (status == null)
-		if (this.status != status) {
-			this.status = status
-			val index = items.indexOfFirst { it is Item.AppInfoItem }
-			if (index >= 0) {
-				if (translate) {
-					notifyItemChanged(index + 1)
-					val from = items.indexOfFirst { it is Item.ReleaseItem }
-					val to = items.indexOfLast { it is Item.ReleaseItem }
-					if (from in 0..to) {
-						notifyItemRangeChanged(from, to - from)
-					}
-				} else {
-					notifyItemChanged(index + 1, Payload.STATUS)
-				}
+	fun setStatus(newStatus: Status) {
+		val statusIndex = items.indexOf(Item.DownloadStatusItem)
+		if (status != newStatus && statusIndex >= 0) {
+			when (newStatus) {
+				is Status.Downloading -> notifyItemChanged(statusIndex, Payload.STATUS)
+				Status.Connecting -> notifyItemChanged(statusIndex, Payload.STATUS)
+				Status.Pending -> notifyItemInserted(statusIndex)
+				Status.Idle -> notifyItemRemoved(statusIndex)
 			}
+			status = newStatus
 		}
 	}
 
@@ -966,7 +952,7 @@ class AppDetailAdapter(private val callbacks: Callbacks) :
 			ViewType.SCREENSHOT -> ScreenShotViewHolder(parent.context)
 			ViewType.SWITCH -> SwitchViewHolder(parent.inflate(R.layout.switch_item)).apply {
 				itemView.setOnClickListener {
-					val switchItem = items[adapterPosition] as Item.SwitchItem
+					val switchItem = items[absoluteAdapterPosition] as Item.SwitchItem
 					val productPreference = when (switchItem.switchType) {
 						SwitchType.IGNORE_ALL_UPDATES -> {
 							ProductPreferences[switchItem.packageName].let { it.copy(ignoreUpdates = !it.ignoreUpdates) }
@@ -991,7 +977,7 @@ class AppDetailAdapter(private val callbacks: Callbacks) :
 			}
 			ViewType.SECTION -> SectionViewHolder(parent.inflate(R.layout.section_item)).apply {
 				itemView.setOnClickListener {
-					val position = adapterPosition
+					val position = absoluteAdapterPosition
 					val sectionItem = items[position] as Item.SectionItem
 					if (sectionItem.items.isNotEmpty()) {
 						expanded += sectionItem.expandType
@@ -1017,7 +1003,7 @@ class AppDetailAdapter(private val callbacks: Callbacks) :
 			}
 			ViewType.EXPAND -> ExpandViewHolder(parent.context).apply {
 				itemView.setOnClickListener {
-					val position = adapterPosition
+					val position = absoluteAdapterPosition
 					val expandItem = items[position] as Item.ExpandItem
 					if (expandItem.expandType !in expanded) {
 						expanded += expandItem.expandType
@@ -1052,20 +1038,20 @@ class AppDetailAdapter(private val callbacks: Callbacks) :
 			ViewType.TEXT -> TextViewHolder(parent.context)
 			ViewType.LINK -> LinkViewHolder(parent.inflate(R.layout.link_item)).apply {
 				itemView.setOnClickListener {
-					val linkItem = items[adapterPosition] as Item.LinkItem
+					val linkItem = items[absoluteAdapterPosition] as Item.LinkItem
 					if (linkItem.uri?.let { callbacks.onUriClick(it, false) } != true) {
 						linkItem.displayLink?.let { copyLinkToClipboard(itemView, it) }
 					}
 				}
 				itemView.setOnLongClickListener {
-					val linkItem = items[adapterPosition] as Item.LinkItem
+					val linkItem = items[absoluteAdapterPosition] as Item.LinkItem
 					linkItem.displayLink?.let { copyLinkToClipboard(itemView, it) }
 					true
 				}
 			}
 			ViewType.PERMISSIONS -> PermissionsViewHolder(parent.inflate(R.layout.permissions_item)).apply {
 				itemView.setOnClickListener {
-					val permissionsItem = items[adapterPosition] as Item.PermissionsItem
+					val permissionsItem = items[absoluteAdapterPosition] as Item.PermissionsItem
 					callbacks.onPermissionsClick(
 						permissionsItem.group?.name,
 						permissionsItem.permissions.map { it.name })
@@ -1073,11 +1059,11 @@ class AppDetailAdapter(private val callbacks: Callbacks) :
 			}
 			ViewType.RELEASE -> ReleaseViewHolder(parent.inflate(R.layout.release_item)).apply {
 				itemView.setOnClickListener {
-					val releaseItem = items[adapterPosition] as Item.ReleaseItem
+					val releaseItem = items[absoluteAdapterPosition] as Item.ReleaseItem
 					callbacks.onReleaseClick(releaseItem.release)
 				}
 				itemView.setOnLongClickListener {
-					val releaseItem = items[adapterPosition] as Item.ReleaseItem
+					val releaseItem = items[absoluteAdapterPosition] as Item.ReleaseItem
 					copyLinkToClipboard(
 						itemView,
 						releaseItem.release.getDownloadUrl(releaseItem.repository)
@@ -1183,10 +1169,10 @@ class AppDetailAdapter(private val callbacks: Callbacks) :
 				item as Item.DownloadStatusItem
 				val status = status
 				holder.statusText.visibility =
-					if (status != null) View.VISIBLE else View.GONE
+					if (status != Status.Idle) View.VISIBLE else View.GONE
 				holder.progress.visibility =
-					if (status != null) View.VISIBLE else View.GONE
-				if (status != null) {
+					if (status != Status.Idle) View.VISIBLE else View.GONE
+				if (status != Status.Idle) {
 					when (status) {
 						is Status.Pending -> {
 							holder.statusText.setText(stringRes.waiting_to_start_download)
@@ -1207,6 +1193,7 @@ class AppDetailAdapter(private val callbacks: Callbacks) :
 									(holder.progress.max.toFloat() * status.read / status.total).roundToInt()
 							} else Unit
 						}
+						Status.Idle -> {}
 					}::class
 				} else {
 				}
@@ -1496,7 +1483,7 @@ class AppDetailAdapter(private val callbacks: Callbacks) :
 					holder.compatibility.text =
 						context.getString(stringRes.only_compatible_with_FORMAT, singlePlatform)
 				}
-				val enabled = status == null
+				val enabled = status == Status.Idle
 				holder.statefulViews.forEach { it.isEnabled = enabled }
 			}
 			ViewType.EMPTY -> {

app/src/main/kotlin/com/looker/droidify/ui/app_detail/AppDetailFragment.kt
@@ -308,24 +308,14 @@ class AppDetailFragment() : ScreenFragment(), AppDetailAdapter.Callbacks {
 		val canShare = product != null && products[0].second.name == "F-Droid"
 
 		val actions = mutableSetOf<Action>()
-		if (canInstall) {
-			actions += Action.INSTALL
-		}
-		if (canUpdate) {
-			actions += Action.UPDATE
-		}
-		if (canLaunch) {
-			actions += Action.LAUNCH
-		}
-		if (installed != null) {
-			actions += Action.DETAILS
-		}
-		if (canUninstall) {
-			actions += Action.UNINSTALL
-		}
-		if (canShare) {
-			actions += Action.SHARE
-		}
+
+		if (canInstall) actions += Action.INSTALL
+		if (canUpdate) actions += Action.UPDATE
+		if (canLaunch) actions += Action.LAUNCH
+		if (installed != null) actions += Action.DETAILS
+		if (canUninstall) actions += Action.UNINSTALL
+		if (canShare) actions += Action.SHARE
+
 		val primaryAction = when {
 			canUpdate -> Action.UPDATE
 			canLaunch -> Action.LAUNCH
@@ -389,9 +379,9 @@ class AppDetailFragment() : ScreenFragment(), AppDetailAdapter.Callbacks {
 				state.read,
 				state.total
 			)
-			else -> null
+			else -> AppDetailAdapter.Status.Idle
 		}
-		val downloading = status != null
+		val downloading = status != AppDetailAdapter.Status.Idle
 		if (this.downloading != downloading) {
 			this.downloading = downloading
 			updateButtons()

app/src/main/kotlin/com/looker/droidify/ui/app_list/AppListAdapter.kt
@@ -122,7 +122,7 @@ class AppListAdapter(private val onClick: (ProductItem) -> Unit) :
 	): RecyclerView.ViewHolder {
 		return when (viewType) {
 			ViewType.PRODUCT -> ProductViewHolder(parent.inflate(R.layout.product_item)).apply {
-				itemView.setOnClickListener { onClick(getProductItem(adapterPosition)) }
+				itemView.setOnClickListener { onClick(getProductItem(absoluteAdapterPosition)) }
 			}
 			ViewType.LOADING -> LoadingViewHolder(parent.context)
 			ViewType.EMPTY -> EmptyViewHolder(parent.context)
@@ -154,6 +154,26 @@ class AppListAdapter(private val onClick: (ProductItem) -> Unit) :
 					error(holder.defaultIcon)
 				}
 				holder.status.apply {
+					if (productItem.installedVersion.nullIfEmpty() != null) {
+						text = productItem.version
+						if (background == null) {
+							background = ResourcesCompat.getDrawable(
+								holder.itemView.resources,
+								R.drawable.background_border,
+								context.theme
+							)
+							resources.sizeScaled(6).let { setPadding(it, it, it, it) }
+							if (productItem.canUpdate) {
+								backgroundTintList =
+									context.getColorFromAttr(R.attr.colorSecondaryContainer)
+								setTextColor(context.getColorFromAttr(R.attr.colorSecondary))
+							} else {
+								backgroundTintList =
+									context.getColorFromAttr(R.attr.colorPrimaryContainer)
+								setTextColor(context.getColorFromAttr(R.attr.colorPrimary))
+							}
+						}
+					}
 					if (productItem.canUpdate) {
 						text = productItem.version
 						if (background == null) {

app/src/main/kotlin/com/looker/droidify/ui/tabs_fragment/TabsFragment.kt
@@ -596,7 +596,7 @@ class TabsFragment : ScreenFragment() {
 			viewType: ViewType,
 		): RecyclerView.ViewHolder {
 			return SectionViewHolder(parent.context).apply {
-				itemView.setOnClickListener { onClick(sections()[adapterPosition]) }
+				itemView.setOnClickListener { onClick(sections()[absoluteAdapterPosition]) }
 			}
 		}
 