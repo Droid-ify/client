f205388b244cbc082f3ba4ac13f5197b3284bf43
LooKeR <mohit2002ss@gmail.com>
2022-11-18 11:11:35 +0530

    Handle exceptions on download request

--- downloader/src/main/java/com/looker/downloader/KtorDownloader.kt
+++ downloader/src/main/java/com/looker/downloader/KtorDownloader.kt
@@ -7,6 +7,9 @@ import com.looker.downloader.model.HeaderInfo
 import io.ktor.client.HttpClient
 import io.ktor.client.call.body
 import io.ktor.client.engine.okhttp.OkHttp
+import io.ktor.client.plugins.ClientRequestException
+import io.ktor.client.plugins.RedirectResponseException
+import io.ktor.client.plugins.ServerResponseException
 import io.ktor.client.plugins.onDownload
 import io.ktor.client.request.get
 import io.ktor.client.statement.HttpResponse
@@ -18,6 +21,7 @@ import kotlinx.coroutines.channels.awaitClose
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.callbackFlow
 import kotlinx.coroutines.flow.flowOn
+import java.io.IOException
 
 object KtorDownloader : Downloader {
 
@@ -28,23 +32,41 @@ object KtorDownloader : Downloader {
 	override suspend fun download(item: DownloadItem): Flow<DownloadState> =
 		callbackFlow {
 			send(DownloadState.Pending)
-			val httpResponse: HttpResponse = httpClient.get(item.url) {
-				onDownload { bytesSent, contentLength ->
-					Log.i(TAG, "download: bytesSent: $bytesSent, contentLength: $contentLength")
-					send(
-						DownloadState.Progress(
-							total = contentLength,
-							percent = bytesSent percentBy contentLength
+			val httpResponse: HttpResponse? = try {
+				httpClient.get(item.url) {
+					onDownload { bytesSent, contentLength ->
+						Log.i(TAG, "download: bytesSent: $bytesSent, contentLength: $contentLength")
+						send(
+							DownloadState.Progress(
+								total = contentLength,
+								percent = bytesSent percentBy contentLength
+							)
 						)
-					)
+					}
 				}
+			} catch (e: RedirectResponseException) {
+				send(DownloadState.Error.HttpError(e.response.status.value, e))
+				null
+			} catch (e: ClientRequestException) {
+				send(DownloadState.Error.HttpError(e.response.status.value, e))
+				null
+			} catch (e: ServerResponseException) {
+				send(DownloadState.Error.HttpError(e.response.status.value, e))
+				null
+			} catch (e: Exception) {
+				send(DownloadState.Error.UnknownError)
+				null
 			}
-			val responseBody: ByteArray = httpResponse.body()
-			item.file.writeBytes(responseBody)
+			val responseBody: ByteArray? = httpResponse?.body()
+			try {
+				responseBody?.let { item.file.writeBytes(it) }
+			} catch (e: IOException) {
+				send(DownloadState.Error.IOError(e))
+			}
 			val headerInfo = HeaderInfo(
-				eTag = httpResponse.etag(),
-				contentLength = httpResponse.contentLength(),
-				lastModified = httpResponse.lastModified()
+				eTag = httpResponse?.etag(),
+				contentLength = httpResponse?.contentLength(),
+				lastModified = httpResponse?.lastModified()
 			)
 			send(DownloadState.Success(headerInfo))
 			awaitClose { println("Cancelled") }
