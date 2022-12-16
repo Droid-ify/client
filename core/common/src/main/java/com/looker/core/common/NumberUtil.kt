b1924349e622ea49b3ffd1d5383978f1facb577b
LooKeR <mohit2002ss@gmail.com>
2022-12-16 20:38:44 +0530

    Fix any 0 / 0 errors

--- core/common/src/main/java/com/looker/core/common/NumberUtil.kt
+++ core/common/src/main/java/com/looker/core/common/NumberUtil.kt
@@ -1,7 +1,9 @@
 package com.looker.core.common
 
-infix fun Int.percentBy(denominator: Int?): Int =
-	this * 100 / (denominator ?: -1)
-
 infix fun Long.percentBy(denominator: Long?): Int =
-	(toInt() * 100) / (denominator ?: -1L).toInt()
+	try {
+		(toInt() * 100) / (denominator ?: -1L).toInt()
+	} catch (e: ArithmeticException) {
+		e.printStackTrace()
+		-1
+	}
