08cd953ea61152fa8b569e050182c0040aeb6d09
LooKeR <mohit2002ss@gmail.com>
2023-02-02 13:41:21 +0530

    Update AGP
    Signed-off-by: LooKeR <mohit2002ss@gmail.com>

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,8 +5,8 @@ buildscript {
 }
 
 plugins {
-	id("com.android.application") version "7.4.0" apply false
-	id("com.android.library") version "7.4.0" apply false
+	id("com.android.application") version "7.4.1" apply false
+	id("com.android.library") version "7.4.1" apply false
 	id("org.jetbrains.kotlin.android") version Version.kotlin apply false
 }
 
