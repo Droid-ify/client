fb799bf92d63dee8089c640ddcaa77880ed2a634
LooKeR <mohit2002ss@gmail.com>
2023-02-09 13:40:46 +0530

    Refactor code
    Signed-off-by: LooKeR <mohit2002ss@gmail.com>

--- app/src/main/kotlin/com/looker/droidify/utility/PackageItemResolver.kt
+++ app/src/main/kotlin/com/looker/droidify/utility/PackageItemResolver.kt
@@ -90,35 +90,29 @@ object PackageItemResolver {
 				Manifest.permission.READ_CONTACTS,
 				Manifest.permission.WRITE_CONTACTS,
 				Manifest.permission.GET_ACCOUNTS,
-				->
-					Manifest.permission_group.CONTACTS
+				-> Manifest.permission_group.CONTACTS
 				Manifest.permission.READ_CALENDAR,
 				Manifest.permission.WRITE_CALENDAR,
-				->
-					Manifest.permission_group.CALENDAR
+				-> Manifest.permission_group.CALENDAR
 				Manifest.permission.SEND_SMS,
 				Manifest.permission.RECEIVE_SMS,
 				Manifest.permission.READ_SMS,
 				Manifest.permission.RECEIVE_MMS,
 				Manifest.permission.RECEIVE_WAP_PUSH,
 				"android.permission.READ_CELL_BROADCASTS",
-				->
-					Manifest.permission_group.SMS
+				-> Manifest.permission_group.SMS
 				Manifest.permission.READ_EXTERNAL_STORAGE,
 				Manifest.permission.WRITE_EXTERNAL_STORAGE,
 				Manifest.permission.ACCESS_MEDIA_LOCATION,
-				->
-					Manifest.permission_group.STORAGE
+				-> Manifest.permission_group.STORAGE
 				Manifest.permission.ACCESS_FINE_LOCATION,
 				Manifest.permission.ACCESS_COARSE_LOCATION,
 				Manifest.permission.ACCESS_BACKGROUND_LOCATION,
-				->
-					Manifest.permission_group.LOCATION
+				-> Manifest.permission_group.LOCATION
 				Manifest.permission.READ_CALL_LOG,
 				Manifest.permission.WRITE_CALL_LOG,
 				@Suppress("DEPRECATION") Manifest.permission.PROCESS_OUTGOING_CALLS,
-				->
-					Manifest.permission_group.CALL_LOG
+				-> Manifest.permission_group.CALL_LOG
 				Manifest.permission.READ_PHONE_STATE,
 				Manifest.permission.READ_PHONE_NUMBERS,
 				Manifest.permission.CALL_PHONE,
@@ -126,16 +120,11 @@ object PackageItemResolver {
 				Manifest.permission.USE_SIP,
 				Manifest.permission.ANSWER_PHONE_CALLS,
 				Manifest.permission.ACCEPT_HANDOVER,
-				->
-					Manifest.permission_group.PHONE
-				Manifest.permission.RECORD_AUDIO ->
-					Manifest.permission_group.MICROPHONE
-				Manifest.permission.ACTIVITY_RECOGNITION ->
-					Manifest.permission_group.ACTIVITY_RECOGNITION
-				Manifest.permission.CAMERA ->
-					Manifest.permission_group.CAMERA
-				Manifest.permission.BODY_SENSORS ->
-					Manifest.permission_group.SENSORS
+				-> Manifest.permission_group.PHONE
+				Manifest.permission.RECORD_AUDIO -> Manifest.permission_group.MICROPHONE
+				Manifest.permission.ACTIVITY_RECOGNITION -> Manifest.permission_group.ACTIVITY_RECOGNITION
+				Manifest.permission.CAMERA -> Manifest.permission_group.CAMERA
+				Manifest.permission.BODY_SENSORS -> Manifest.permission_group.SENSORS
 				else -> null
 			}
 		} else permissionInfo.group
