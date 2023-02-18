28f4f0555acd88a871152d09664fce5551686d0b
LooKeR <mohit2002ss@gmail.com>
2023-02-18 14:09:18 +0530

    Release v0.5.6
    Signed-off-by: LooKeR <mohit2002ss@gmail.com>

--- buildSrc/src/main/kotlin/Android.kt
+++ buildSrc/src/main/kotlin/Android.kt
@@ -2,6 +2,6 @@ object Android {
 	const val appId = "com.looker.droidify"
 	const val compileSdk = 33
 	const val minSdk = 23
-	const val versionCode = 55
-	const val versionName = "0.5.5"
+	const val versionCode = 56
+	const val versionName = "0.5.6"
 }
