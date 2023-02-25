76ec9b38d211b9b943b50f4ab1dc98bd902b8684
LooKeR <mohit2002ss@gmail.com>
2023-02-25 14:15:03 +0530

    Add FAB to `Add Repository` (Closes #274)
    Signed-off-by: LooKeR <mohit2002ss@gmail.com>

--- app/src/main/kotlin/com/looker/droidify/screen/RepositoriesFragment.kt
+++ app/src/main/kotlin/com/looker/droidify/screen/RepositoriesFragment.kt
@@ -3,26 +3,27 @@ package com.looker.droidify.screen
 import android.database.Cursor
 import android.os.Bundle
 import android.view.LayoutInflater
-import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
+import com.looker.core.common.extension.dp
+import com.looker.core.common.extension.systemBarsMargin
 import com.looker.core.common.extension.systemBarsPadding
 import com.looker.droidify.database.CursorOwner
+import com.looker.droidify.databinding.RecyclerViewWithFabBinding
 import com.looker.droidify.service.Connection
 import com.looker.droidify.service.SyncService
-import com.looker.droidify.utility.Utils
 import com.looker.droidify.utility.extension.resources.sizeScaled
 import com.looker.droidify.utility.extension.screenActivity
 import com.looker.droidify.widget.DividerItemDecoration
 import dagger.hilt.android.AndroidEntryPoint
-import com.looker.droidify.R.drawable as drawableRes
-import com.looker.droidify.R.string as stringRes
+import com.looker.core.common.R as CommonR
 
 @AndroidEntryPoint
 class RepositoriesFragment : ScreenFragment(), CursorOwner.Callback {
-	private var recyclerView: RecyclerView? = null
+
+	private var _binding: RecyclerViewWithFabBinding? = null
+	private val binding get() = _binding!!
 
 	private val syncConnection = Connection(SyncService::class.java)
 
@@ -32,21 +33,26 @@ class RepositoriesFragment : ScreenFragment(), CursorOwner.Callback {
 		savedInstanceState: Bundle?,
 	): View {
 		super.onCreateView(inflater, container, savedInstanceState)
+		_binding = RecyclerViewWithFabBinding.inflate(inflater, container, false)
 		val view = fragmentBinding.root.apply {
-			val content = fragmentBinding.fragmentContent
-			content.addView(
-				RecyclerView(content.context).apply {
-					id = android.R.id.list
-					layoutManager = LinearLayoutManager(context)
-					isMotionEventSplittingEnabled = false
-					setHasFixedSize(true)
-					adapter = RepositoriesAdapter(
-						navigate = { screenActivity.navigateRepository(it.id) }
-					) { repository, isEnabled ->
-						repository.enabled != isEnabled &&
-								syncConnection.binder?.setEnabled(repository, isEnabled) == true
-					}
-					addItemDecoration(DividerItemDecoration(context) { context, _, configuration ->
+			binding.scrollUp.apply {
+				setIconResource(CommonR.drawable.ic_add)
+				setText(CommonR.string.add_repository)
+				setOnClickListener { screenActivity.navigateAddRepository() }
+				systemBarsMargin(16.dp)
+			}
+			binding.recyclerView.apply {
+				layoutManager = LinearLayoutManager(context)
+				isMotionEventSplittingEnabled = false
+				setHasFixedSize(true)
+				adapter = RepositoriesAdapter(
+					navigate = { screenActivity.navigateRepository(it.id) }
+				) { repository, isEnabled ->
+					repository.enabled != isEnabled &&
+							syncConnection.binder?.setEnabled(repository, isEnabled) == true
+				}
+				addItemDecoration(
+					DividerItemDecoration(context) { _, _, configuration ->
 						val padding = context.resources.sizeScaled(16)
 						configuration.set(
 							needDivider = true,
@@ -54,13 +60,21 @@ class RepositoriesFragment : ScreenFragment(), CursorOwner.Callback {
 							paddingStart = padding,
 							paddingEnd = padding
 						)
-					})
-					recyclerView = this
-				}
-			)
+					}
+				)
+				systemBarsPadding()
+			}
+			fragmentBinding.fragmentContent.addView(binding.root)
 		}
-		recyclerView?.systemBarsPadding()
+		handleFab()
 		return view
+	}
+
+	private fun handleFab() {
+		binding.recyclerView.setOnScrollChangeListener { _, _, scrollY, _, oldScrollY ->
+			if (scrollY > oldScrollY) binding.scrollUp.shrink()
+			else binding.scrollUp.extend()
+		}
 	}
 
 	override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
@@ -69,26 +83,18 @@ class RepositoriesFragment : ScreenFragment(), CursorOwner.Callback {
 		syncConnection.bind(requireContext())
 		screenActivity.cursorOwner.attach(this, CursorOwner.Request.Repositories)
 		screenActivity.onToolbarCreated(toolbar)
-		toolbar.menu.add(stringRes.add_repository)
-			.setIcon(Utils.getToolbarIcon(toolbar.context, drawableRes.ic_add))
-			.setShowAsActionFlags(MenuItem.SHOW_AS_ACTION_ALWAYS)
-			.setOnMenuItemClickListener {
-				view.post { screenActivity.navigateAddRepository() }
-				true
-			}
-		collapsingToolbar.title = getString(stringRes.repositories)
+		collapsingToolbar.title = getString(CommonR.string.repositories)
 	}
 
 	override fun onDestroyView() {
 		super.onDestroyView()
 
-		recyclerView = null
-
+		_binding = null
 		syncConnection.unbind(requireContext())
 		screenActivity.cursorOwner.detach(this)
 	}
 
 	override fun onCursorData(request: CursorOwner.Request, cursor: Cursor?) {
-		(recyclerView?.adapter as? RepositoriesAdapter)?.cursor = cursor
+		(binding.recyclerView.adapter as RepositoriesAdapter).cursor = cursor
 	}
 }
