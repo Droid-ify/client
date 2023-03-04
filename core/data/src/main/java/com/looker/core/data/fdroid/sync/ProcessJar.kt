b561e7f8e41d5796bbc59164ccc3b4a6a7c749f1
LooKeR <mohit2002ss@gmail.com>
2023-03-04 13:36:21 +0530

    Fix the issue with `JarFile::getFingerprint`
    Signed-off-by: LooKeR <mohit2002ss@gmail.com>

--- core/data/src/main/java/com/looker/core/data/fdroid/sync/ProcessJar.kt
+++ core/data/src/main/java/com/looker/core/data/fdroid/sync/ProcessJar.kt
@@ -7,6 +7,7 @@ import java.security.cert.X509Certificate
 import java.util.jar.JarEntry
 import java.util.jar.JarFile
 import kotlin.coroutines.resume
+import kotlin.coroutines.resumeWithException
 
 suspend fun JarFile.getIndexV1(): IndexV1 = suspendCancellableCoroutine { cont ->
 	val indexEntry = getEntry(IndexType.INDEX_V1.contentName) as JarEntry
@@ -14,17 +15,32 @@ suspend fun JarFile.getIndexV1(): IndexV1 = suspendCancellableCoroutine { cont -
 	cont.resume(indexFile)
 }
 
-suspend fun JarFile.getFingerprint(): String = suspendCancellableCoroutine { cont ->
-	val indexEntry = getEntry(IndexType.INDEX_V1.contentName) as JarEntry
+suspend fun JarFile.getFingerprint(
+	indexType: IndexType = IndexType.INDEX_V1
+): String = suspendCancellableCoroutine { cont ->
+	val indexEntry = getEntry(indexType.contentName) as JarEntry
 	val codeSigners = indexEntry.codeSigners
-	if (codeSigners == null || codeSigners.size != 1) throw ProcessJarException("index.jar must be signed by a single code signer")
+	if (codeSigners == null || codeSigners.size != 1) {
+		cont.resumeWithException(
+			ProcessJarException("index.jar must be signed by a single code signer")
+		)
+	}
+	if (cont.isCompleted) return@suspendCancellableCoroutine
 	val certificates = codeSigners[0].signerCertPath?.certificates.orEmpty()
-	if (certificates.size != 1) throw ProcessJarException("index.jar code signer should have only one certificate")
+	if (certificates.size != 1) {
+		cont.resumeWithException(
+			ProcessJarException("index.jar code signer should have only one certificate")
+		)
+	}
+	if (cont.isCompleted) return@suspendCancellableCoroutine
 	val fingerprint = (certificates[0] as X509Certificate).fingerprint()
 	cont.resume(fingerprint)
 }
 
-internal enum class IndexType(val jarName: String, val contentName: String) {
+enum class IndexType(
+	val jarName: String,
+	val contentName: String
+) {
 	INDEX_V1("index-v1.jar", "index-v1.json")
 }
 
