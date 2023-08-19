1c169028820c3395f86f2e211030e3817f3c0c20
LooKeR <mohit2002ss@gmail.com>
2023-08-20 01:40:05 +0530

    Release v0.5.8

--- buildSrc/src/main/kotlin/Android.kt
+++ buildSrc/src/main/kotlin/Android.kt
@@ -2,6 +2,6 @@ object Android {
 	const val appId = "com.looker.droidify"
 	const val compileSdk = 33
 	const val minSdk = 23
-	const val versionCode = 57
-	const val versionName = "0.5.7"
+	const val versionCode = 58
+	const val versionName = "0.5.8"
 }
