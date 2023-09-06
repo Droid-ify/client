3368bddde4667b4d741f5e0be3a9de6f13b0e2c2
LooKeR <mohit2002ss@gmail.com>
2023-09-06 09:44:00 +0530

    Complete migration Still need some work to perfect this impl. Will consider using version catalog in future

---
+++ build-logic/structure/src/main/kotlin/AndroidApplicationPlugin.kt
@@ -0,0 +1,22 @@
+import com.android.build.api.dsl.ApplicationExtension
+import com.looker.droidify.configureKotlinAndroid
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+import org.gradle.kotlin.dsl.configure
+
+class AndroidApplicationPlugin : Plugin<Project> {
+	override fun apply(target: Project) {
+		with(target) {
+			with(pluginManager) {
+				apply("com.android.application")
+				apply("org.jetbrains.kotlin.android")
+				apply("nowinandroid.android.lint")
+			}
+
+			extensions.configure<ApplicationExtension> {
+				configureKotlinAndroid(this)
+				defaultConfig.targetSdk = DefaultConfig.compileSdk
+			}
+		}
+	}
+}
