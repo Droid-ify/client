6d5907ab480bfaed13f9fe99e9b528b0a0201611
LooKeR <mohit2002ss@gmail.com>
2023-09-10 00:38:37 +0530

    Cap max direct file size to 25 mb Direct read size is reduced which fixes the `OutOfMemoryError` (Closes #475)

--- core/common/src/main/java/com/looker/core/common/signature/HashChecker.kt
+++ core/common/src/main/java/com/looker/core/common/signature/HashChecker.kt
@@ -35,24 +35,25 @@ private suspend fun MessageDigest.readBytesFrom(
 	file: File
 ): ByteArray? = withContext(Dispatchers.IO) {
 	try {
-		if (file.length() < Int.MAX_VALUE) digest(file.readBytes())
-		else {
-			val buffer = ByteArray(DEFAULT_BUFFER_SIZE)
-			file.inputStream().use { input ->
-				var bytesRead = input.read(buffer)
-				while (bytesRead >= 0) {
-					ensureActive()
-					update(buffer, 0, bytesRead)
-					bytesRead = input.read(buffer)
-				}
-				digest()
+		if (file.length() < DIRECT_READ_LIMIT) return@withContext digest(file.readBytes())
+		val buffer = ByteArray(DEFAULT_BUFFER_SIZE)
+		file.inputStream().use { input ->
+			var bytesRead = input.read(buffer)
+			while (bytesRead >= 0) {
+				ensureActive()
+				update(buffer, 0, bytesRead)
+				bytesRead = input.read(buffer)
 			}
+			digest()
 		}
 	} catch (e: Exception) {
 		e.exceptCancellation()
 		null
 	}
 }
+
+// 25 MB
+private const val DIRECT_READ_LIMIT = 25 * 1024 * 1024
 
 @Suppress("FunctionName")
 data class Hash(
