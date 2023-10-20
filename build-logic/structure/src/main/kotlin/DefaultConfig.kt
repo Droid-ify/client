837414d62e21f91dbff01ca7f86c501c15d38ed6
LooKeR <mohit2002ss@gmail.com>
2023-10-20 21:15:42 +0530

    Release 0.5.9 Patch 3

--- build-logic/structure/src/main/kotlin/DefaultConfig.kt
+++ build-logic/structure/src/main/kotlin/DefaultConfig.kt
@@ -4,6 +4,6 @@ object DefaultConfig {
 	const val appId = "com.looker.droidify"
 	const val compileSdk = 34
 	const val minSdk = 23
-	const val versionCode = 592
-	const val versionName = "0.5.9 Patch 2"
+	const val versionCode = 593
+	const val versionName = "0.5.9 Patch 3"
 }
