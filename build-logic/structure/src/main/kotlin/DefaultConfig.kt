2d792c812438e2388b794de5804f20a0b88a6284
LooKeR <mohit2002ss@gmail.com>
2024-01-07 13:10:51 +0530

    Release 0.6.1

--- build-logic/structure/src/main/kotlin/DefaultConfig.kt
+++ build-logic/structure/src/main/kotlin/DefaultConfig.kt
@@ -4,6 +4,6 @@ object DefaultConfig {
     const val appId = "com.looker.droidify"
     const val compileSdk = 34
     const val minSdk = 23
-    const val versionCode = 600
-    const val versionName = "0.6.0"
+    const val versionCode = 610
+    const val versionName = "0.6.1"
 }
