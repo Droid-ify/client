43776be617656ddffe859488e333271c66440a0a
LooKeR <mohit2002ss@gmail.com>
2024-03-22 23:47:33 +0530

    Some modifications towards new arch.

--- core/domain/src/main/kotlin/com/looker/core/domain/Syncable.kt
+++ core/domain/src/main/kotlin/com/looker/core/domain/Syncable.kt
@@ -3,12 +3,28 @@ package com.looker.core.domain
 import com.looker.core.domain.newer.App
 import com.looker.core.domain.newer.Repo
 
+/**
+ * Expected Architecture: https://excalidraw.com/#json=6JIt5NYdqesMGza45l5eE,uyCOnQUx2ET8sVsmtJivjg
+ *
+ * Current Issue: When downloading entry.jar we need to re-call the synchronizer,
+ * which this arch doesn't allow.
+ */
 interface Syncable {
 
-    val repo: Repo
+    val synchronizer: Synchronizer
 
-    suspend fun getApps(): List<App>
+    val parser: Parser
 
-    suspend fun getUpdatedRepo(): Repo
+}
 
+interface Parser {
+
+    suspend fun parsedRepo(): Repo
+
+    suspend fun parsedApps(): List<App>
+
+}
+
+interface Synchronizer {
+    suspend fun downloadData()
 }
---
+++ sync/fdroid/src/main/kotlin/com/looker/sync/fdroid/IndexType.kt
@@ -0,0 +1,8 @@
+package com.looker.sync.fdroid
+
+// Index-v1
+// Index-v2
+// Github / Gitlab
+enum class IndexType {
+
+}
