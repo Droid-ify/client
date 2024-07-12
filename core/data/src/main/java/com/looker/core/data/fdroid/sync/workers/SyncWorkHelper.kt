66fcf7b4ca4d29df76b6663176c54b6a19065be6
LooKeR <mohit2002ss@gmail.com>
2024-07-13 00:04:43 +0530

    Add Foreground Service Type for SyncWorker

--- core/data/src/main/java/com/looker/core/data/fdroid/sync/workers/SyncWorkHelper.kt
+++ core/data/src/main/java/com/looker/core/data/fdroid/sync/workers/SyncWorkHelper.kt
@@ -2,6 +2,8 @@ package com.looker.core.data.fdroid.sync.workers
 
 import android.app.Notification
 import android.content.Context
+import android.content.pm.ServiceInfo.FOREGROUND_SERVICE_TYPE_DATA_SYNC
+import android.os.Build
 import androidx.core.app.NotificationCompat
 import androidx.work.ForegroundInfo
 import com.looker.core.common.createNotificationChannel
@@ -10,10 +12,18 @@ import com.looker.core.common.R as CommonR
 private const val SyncNotificationID = 12
 private const val SyncNotificationChannelID = "SyncNotificationChannelID"
 
-fun Context.syncForegroundInfo() = ForegroundInfo(
-    SyncNotificationID,
-    syncWorkNotification()
-)
+fun Context.syncForegroundInfo() = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+    ForegroundInfo(
+        SyncNotificationID,
+        syncWorkNotification(),
+        FOREGROUND_SERVICE_TYPE_DATA_SYNC,
+    )
+} else {
+    ForegroundInfo(
+        SyncNotificationID,
+        syncWorkNotification(),
+    )
+}
 
 private fun Context.syncWorkNotification(): Notification {
     createNotificationChannel(
