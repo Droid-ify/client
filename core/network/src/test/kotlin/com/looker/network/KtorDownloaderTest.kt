c645866fee03684866e9eea356fbac51ad6c76b7
LooKeR <mohit2002ss@gmail.com>
2024-07-13 12:57:31 +0530

    Move to `kotlin` sourceSet

---
+++ core/network/src/test/kotlin/com/looker/network/KtorDownloaderTest.kt
@@ -0,0 +1,18 @@
+package com.looker.network
+
+import org.junit.jupiter.api.Test
+
+class KtorDownloaderTest {
+
+    @Test
+    fun setProxy() {
+    }
+
+    @Test
+    fun headCall() {
+    }
+
+    @Test
+    fun downloadToFile() {
+    }
+}
