5ddb3f55e761641183bf4eaa9b42bfc8f1ec4e18
LooKeR <mohit2002ss@gmail.com>
2024-07-14 11:42:45 +0530

    Make `di` jvm library

--- core/di/build.gradle.kts
+++ core/di/build.gradle.kts
@@ -1,9 +1,10 @@
 plugins {
-    alias(libs.plugins.looker.android.library)
+    alias(libs.plugins.looker.jvm.library)
     alias(libs.plugins.looker.hilt)
     alias(libs.plugins.looker.lint)
 }
 
-android {
-    namespace = "com.looker.core.di"
+dependencies {
+    implementation(libs.kotlinx.coroutines.core)
+
 }
