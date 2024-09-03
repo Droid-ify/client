6c59e24fe170bc637947436b0739671885357aba
machiav3lli <antonios.hazim@tutanota.com>
2024-09-04 00:55:43 +0200

    Add: Diff data classes and relevant patch functions

--- sync/fdroid/src/main/kotlin/com/looker/sync/fdroid/v2/model/RepoV2.kt
+++ sync/fdroid/src/main/kotlin/com/looker/sync/fdroid/v2/model/RepoV2.kt
@@ -15,6 +15,55 @@ data class RepoV2(
 )
 
 @Serializable
+data class RepoV2Diff(
+    val address: String? = null,
+    val icon: LocalizedIcon? = null,
+    val name: LocalizedString? = null,
+    val description: LocalizedString? = null,
+    val antiFeatures: Map<String, AntiFeatureV2?>? = null,
+    val categories: Map<String, CategoryV2?>? = null,
+    val mirrors: List<MirrorV2>? = null,
+    val timestamp: Long,
+) {
+    fun patchInto(repo: RepoV2): RepoV2 {
+        val (antiFeaturesToRemove, antiFeaturesToAdd) = (antiFeatures?.entries
+            ?.partition { it.value == null }
+            ?: Pair(emptyList(), emptyList()))
+            .let {
+                Pair(
+                    it.first.map { entry -> entry.key }.toSet(),
+                    it.second.mapNotNull { (key, value) -> value?.let { key to value } }
+                )
+            }
+
+        val (categoriesToRemove, categoriesToAdd) = (categories?.entries
+            ?.partition { it.value == null }
+            ?: Pair(emptyList(), emptyList()))
+            .let {
+                Pair(
+                    it.first.map { entry -> entry.key }.toSet(),
+                    it.second.mapNotNull { (key, value) -> value?.let { key to value } }
+                )
+            }
+
+        return repo.copy(
+            timestamp = timestamp,
+            address = address ?: repo.address,
+            icon = icon ?: repo.icon,
+            name = name ?: repo.name,
+            description = description ?: repo.description,
+            mirrors = mirrors ?: repo.mirrors,
+            antiFeatures = repo.antiFeatures
+                .minus(antiFeaturesToRemove)
+                .plus(antiFeaturesToAdd),
+            categories = repo.categories
+                .minus(categoriesToRemove)
+                .plus(categoriesToAdd),
+        )
+    }
+}
+
+@Serializable
 data class MirrorV2(
     val url: String,
     val isPrimary: Boolean? = null,
