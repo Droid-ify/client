c7849df0ca79bb326644bc67eebf94b446a4c0dd
LooKeR <mohit2002ss@gmail.com>
2024-12-21 16:53:10 +0530

    Upgrade sdk to 25

--- build-logic/structure/src/main/kotlin/DefaultConfig.kt
+++ build-logic/structure/src/main/kotlin/DefaultConfig.kt
@@ -1,8 +1,8 @@
 object DefaultConfig {
     // Update [release_build.yml] along with this
-    const val buildTools: String = "34.0.0"
+    const val buildTools: String = "35.0.0"
     const val appId = "com.looker.droidify"
-    const val compileSdk = 34
+    const val compileSdk = 35
     const val minSdk = 23
     // Check for TODOs before update
     const val versionCode = 630
