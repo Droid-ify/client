f121b22dd3df8636606a7bcd1f1b5ba4f149de2f
LooKeR <mohit2002ss@gmail.com>
2025-02-12 15:23:34 +0530

    perf: Replace enum with IntDef
    Trade type-safety for performance by replacing enum with int def

--- app/src/main/kotlin/com/looker/droidify/data/local/model/ScreenshotEntity.kt
+++ app/src/main/kotlin/com/looker/droidify/data/local/model/ScreenshotEntity.kt
@@ -1,5 +1,6 @@
 package com.looker.droidify.data.local.model
 
+import androidx.annotation.IntDef
 import androidx.room.Entity
 import androidx.room.ForeignKey
 import androidx.room.ForeignKey.Companion.CASCADE
@@ -23,56 +24,76 @@ import com.looker.droidify.sync.v2.model.ScreenshotsV2
 )
 data class ScreenshotEntity(
     val path: String,
-    val type: ScreenshotType,
+    @ScreenshotType
+    val type: Int,
     val locale: String,
     val appId: Int,
 )
-
-enum class ScreenshotType {
-    Phone, SevenInch, TenInch, Wear, Tv
-}
 
 fun ScreenshotsV2.localizedScreenshots(appId: Int): List<ScreenshotEntity> {
     if (isNull) return emptyList()
     val screenshots = mutableListOf<ScreenshotEntity>()
 
-    val screenshotIterator: (ScreenshotType, LocalizedFiles) -> Unit = { type, localizedFiles ->
-        localizedFiles.forEach { (locale, files) ->
+    val screenshotIterator: (Int, LocalizedFiles?) -> Unit = { type, localizedFiles ->
+        localizedFiles?.forEach { (locale, files) ->
             for ((path, _, _) in files) {
-                val entity = ScreenshotEntity(
-                    locale = locale,
-                    appId = appId,
-                    type = type,
-                    path = path,
+                screenshots.add(
+                    ScreenshotEntity(
+                        locale = locale,
+                        appId = appId,
+                        type = type,
+                        path = path,
+                    )
                 )
-                screenshots.add(entity)
             }
         }
     }
-
-    phone?.let { screenshotIterator(ScreenshotType.Phone, it) }
-    sevenInch?.let { screenshotIterator(ScreenshotType.SevenInch, it) }
-    tenInch?.let { screenshotIterator(ScreenshotType.TenInch, it) }
-    wear?.let { screenshotIterator(ScreenshotType.Wear, it) }
-    tv?.let { screenshotIterator(ScreenshotType.Tv, it) }
-
+    screenshotIterator(PHONE, phone)
+    screenshotIterator(SEVEN_INCH, sevenInch)
+    screenshotIterator(TEN_INCH, tenInch)
+    screenshotIterator(WEAR, wear)
+    screenshotIterator(TV, tv)
     return screenshots
 }
 
 fun List<ScreenshotEntity>.toScreenshots(): Screenshots {
-    val screenshotMap = HashMap<ScreenshotType, MutableList<String>>(5)
-    ScreenshotType.entries.forEach { type ->
-        screenshotMap[type] = mutableListOf()
-    }
-    for (entity in this) {
-        screenshotMap[entity.type]!!.add(entity.path)
+    val phone = mutableListOf<String>()
+    val sevenInch = mutableListOf<String>()
+    val tenInch = mutableListOf<String>()
+    val wear = mutableListOf<String>()
+    val tv = mutableListOf<String>()
+    for (index in this.indices) {
+        val entity = get(index)
+        when (entity.type) {
+            PHONE -> phone.add(entity.path)
+            SEVEN_INCH -> sevenInch.add(entity.path)
+            TEN_INCH -> tenInch.add(entity.path)
+            TV -> tv.add(entity.path)
+            WEAR -> wear.add(entity.path)
+        }
     }
 
     return Screenshots(
-        phone = screenshotMap[ScreenshotType.Phone]!!,
-        sevenInch = screenshotMap[ScreenshotType.SevenInch]!!,
-        tenInch = screenshotMap[ScreenshotType.TenInch]!!,
-        wear = screenshotMap[ScreenshotType.Wear]!!,
-        tv = screenshotMap[ScreenshotType.Tv]!!,
+        phone = phone,
+        sevenInch = sevenInch,
+        tenInch = tenInch,
+        wear = wear,
+        tv = tv,
     )
 }
+
+@Retention(AnnotationRetention.BINARY)
+@IntDef(
+    PHONE,
+    SEVEN_INCH,
+    TEN_INCH,
+    WEAR,
+    TV,
+)
+private annotation class ScreenshotType
+
+private const val PHONE = 0
+private const val SEVEN_INCH = 1
+private const val TEN_INCH = 2
+private const val WEAR = 3
+private const val TV = 4
