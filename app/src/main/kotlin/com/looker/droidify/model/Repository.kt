73570e803c13b277fa0c4210a505120ab6afbc3d
jwkwshjsjsj <eetutorma1@gmail.com>
2025-07-10 23:20:33 +0300

    Update Repository.kt

--- app/src/main/kotlin/com/looker/droidify/model/Repository.kt
+++ app/src/main/kotlin/com/looker/droidify/model/Repository.kt
@@ -107,7 +107,7 @@ data class Repository(
             ),
             defaultRepository(
                 address = "https://guardianproject.info/fdroid/repo",
-                name = "Guardian Project Official Releases",
+                name = "Guardian Project official releases",
                 description = "The official repository of The Guardian " +
                     "Project apps for use with the F-Droid client. Appl" +
                     "ications in this repository are official binaries " +
@@ -126,7 +126,7 @@ data class Repository(
             ),
             defaultRepository(
                 address = "https://apt.izzysoft.de/fdroid/repo",
-                name = "IzzyOnDroid F-Droid Repo",
+                name = "IzzyOnDroid F-Droid repository",
                 description = "This is a repository of apps to be used with" +
                     " F-Droid the original application developers, taken" +
                     " from the responsible repositories. At thi" +
@@ -179,13 +179,13 @@ data class Repository(
             ),
             defaultRepository(
                 address = "https://calyxos.gitlab.io/calyx-fdroid-repo/fdroid/repo",
-                name = "Calyx OS Repo",
+                name = "CalyxOS repository",
                 description = "The official Calyx Labs F-Droid repository.",
                 fingerprint = "C44D58B4547DE5096138CB0B34A1CC99DAB3B4274412ED753FCCBFC11DC1B7B6"
             ),
             defaultRepository(
                 address = "https://divestos.org/fdroid/official",
-                name = "Divest OS Repo",
+                name = "DivestOS repository",
                 description = "The official DivestOS F-Droid repository.",
                 fingerprint = "E4BE8D6ABFA4D9D4FEEF03CDDA7FF62A73FD64B75566F6DD4E5E577550BE8467"
             ),
@@ -243,7 +243,7 @@ data class Repository(
                 name = "Threema Libre",
                 description = "The official repository for Threema Libre. R" +
                     "equires Threema Shop license. Threema Libre is an open" +
-                    "-source messenger focused on security and privacy.",
+                    "source messenger focused on security and privacy.",
                 fingerprint = "5734E753899B25775D90FE85362A49866E05AC4F83C05BEF5A92880D2910639E"
             ),
             defaultRepository(
@@ -289,28 +289,28 @@ data class Repository(
             ),
             defaultRepository(
                 address = "https://s2.spiritcroc.de/testing/fdroid/repo",
-                name = "SpiritCroc.de Test Builds",
-                description = "SpiritCroc.de Test Builds",
+                name = "SpiritCroc.de test builds",
+                description = "SpiritCroc.de test builds",
                 fingerprint = "52d03f2fab785573bb295c7ab270695e3a1bdd2adc6a6de8713250b33f231225"
             ),
             defaultRepository(
                 address = "https://static.cryptomator.org/android/fdroid/repo",
                 name = "Cryptomator",
-                description = "No Description",
+                description = "No description.",
                 fingerprint = "f7c3ec3b0d588d3cb52983e9eb1a7421c93d4339a286398e71d7b651e8d8ecdd"
             ),
             defaultRepository(
                 address = "https://divestos.org/apks/unofficial/fdroid/repo",
                 name = "DivestOS Unofficial",
                 description = "This repository contains unofficial builds of open source apps" +
-                    " that are not included in the other repos.",
+                    " that are not included in the other repositories.",
                 fingerprint = "a18cdb92f40ebfbbf778a54fd12dbd74d90f1490cb9ef2cc6c7e682dd556855d"
             ),
             defaultRepository(
                 address = "https://cdn.kde.org/android/stable-releases/fdroid/repo",
                 name = "KDE Stables",
                 description = "This repository contains unofficial builds of open source apps" +
-                    " that are not included in the other repos.",
+                    " that are not included in the other repositories.",
                 fingerprint = "13784ba6c80ff4e2181e55c56f961eed5844cea16870d3b38d58780b85e1158f"
             ),
             defaultRepository(
@@ -322,7 +322,7 @@ data class Repository(
             ),
             defaultRepository(
                 address = "https://app.simplex.chat/fdroid/repo",
-                name = "SimpleX Chat F-Droid",
+                name = "SimpleX Chat",
                 description = "SimpleX Chat official F-Droid repository.",
                 fingerprint = "9F358FF284D1F71656A2BFAF0E005DEAE6AA14143720E089F11FF2DDCFEB01BA"
             ),
@@ -335,7 +335,7 @@ data class Repository(
             defaultRepository(
                 address = "https://fdroid.cakelabs.com/fdroid/repo",
                 name = "Cake Labs",
-                description = "Cake Labs official F-Droid repository for Cake Wallet and Monero.com",
+                description = "Cake Labs official F-Droid repository for Cake Wallet and Monero.com.",
                 fingerprint = "EA44EFAEE0B641EE7A032D397D5D976F9C4E5E1ED26E11C75702D064E55F8755"
             ),
             defaultRepository(
@@ -347,19 +347,19 @@ data class Repository(
             defaultRepository(
                 address = "https://fdroid.mm20.de/repo",
                 name = "MM20 Apps",
-                description = "Apps developed and distributed by MM20",
+                description = "Apps developed and distributed by MM20.",
                 fingerprint = "156FBAB952F6996415F198F3F29628D24B30E725B0F07A2B49C3A9B5161EEE1A"
             ),
             defaultRepository(
                 address = "https://breezy-weather.github.io/fdroid-repo/fdroid/repo",
                 name = "Breezy Weather",
-                description = "The F-Droid repository for Breezy Weather",
+                description = "The F-Droid repository for Breezy Weather.",
                 fingerprint = "3480A7BB2A296D8F98CB90D2309199B5B9519C1B31978DBCD877ADB102AF35EE"
             ),
             defaultRepository(
                 address = "https://gh.artemchep.com/keyguard-repo-fdroid/repo",
                 name = "Keyguard Project",
-                description = "Mirrors artifacts available on https://github.com/AChep/keyguard-app/releases",
+                description = "Mirrors artifacts available on https://github.com/AChep/keyguard-app/releases.",
                 fingerprint = "03941CE79B081666609C8A48AB6E46774263F6FC0BBF1FA046CCFFC60EA643BC"
             ),
             defaultRepository(
