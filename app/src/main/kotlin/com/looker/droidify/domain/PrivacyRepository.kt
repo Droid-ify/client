77ac568acfab3656f556fa5bf625af2883cf02aa
machiav3lli <antonios.hazim@tutanota.com>
2025-07-12 12:51:37 +0200

    Add: API and repository for RB logs

---
+++ app/src/main/kotlin/com/looker/droidify/domain/PrivacyRepository.kt
@@ -0,0 +1,23 @@
+package com.looker.droidify.domain
+
+import com.looker.droidify.data.local.dao.RBLogDao
+import com.looker.droidify.data.local.model.RBLogEntity
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.SupervisorJob
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flowOn
+import kotlinx.coroutines.withContext
+
+class PrivacyRepository(val rbDao: RBLogDao) {
+    private val cc = Dispatchers.IO
+    private val jcc = Dispatchers.IO + SupervisorJob()
+
+    fun getRBLogs(packageName: String): Flow<List<RBLogEntity>> = rbDao.getFlow(packageName)
+        .flowOn(cc)
+
+    suspend fun upsertRBLogs(vararg logs: RBLogEntity) {
+        withContext(jcc) {
+            rbDao.upsert(*logs)
+        }
+    }
+}
