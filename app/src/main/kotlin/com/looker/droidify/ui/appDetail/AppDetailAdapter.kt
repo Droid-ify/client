eda9ace3286ed0aea9fcc8559f7e611f6a87386d
LooKeR <iamlooker@proton.me>
2025-11-05 12:27:32 +0530

    revert: AppDetailAdapter color changes

--- app/src/main/kotlin/com/looker/droidify/ui/appDetail/AppDetailAdapter.kt
+++ app/src/main/kotlin/com/looker/droidify/ui/appDetail/AppDetailAdapter.kt
@@ -144,9 +144,9 @@ class AppDetailAdapter(private val callbacks: Callbacks) :
 
     private enum class SectionType(
         val titleResId: Int,
-        val colorAttrResId: Int = MaterialR.attr.colorPrimaryContainer
+        val colorAttrResId: Int = MaterialR.attr.colorPrimary
     ) {
-        ANTI_FEATURES(stringRes.anti_features, MaterialR.attr.colorErrorContainer),
+        ANTI_FEATURES(stringRes.anti_features, MaterialR.attr.colorError),
         CHANGES(stringRes.changes),
         LINKS(stringRes.links),
         DONATE(stringRes.donate),
@@ -427,10 +427,10 @@ class AppDetailAdapter(private val callbacks: Callbacks) :
     private class InstallButtonViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         val button = itemView.findViewById<MaterialButton>(R.id.action)!!
 
-        val actionTintNormal = button.context.getColorFromAttr(MaterialR.attr.colorPrimaryContainer)
-        val actionTintOnNormal = button.context.getColorFromAttr(MaterialR.attr.colorOnPrimaryContainer)
-        val actionTintCancel = button.context.getColorFromAttr(MaterialR.attr.colorErrorContainer)
-        val actionTintOnCancel = button.context.getColorFromAttr(MaterialR.attr.colorOnErrorContainer)
+        val actionTintNormal = button.context.getColorFromAttr(MaterialR.attr.colorPrimary)
+        val actionTintOnNormal = button.context.getColorFromAttr(MaterialR.attr.colorOnPrimary)
+        val actionTintCancel = button.context.getColorFromAttr(MaterialR.attr.colorError)
+        val actionTintOnCancel = button.context.getColorFromAttr(MaterialR.attr.colorOnError)
         val actionTintDisabled = button.context.getColorFromAttr(MaterialR.attr.colorOutline)
         val actionTintOnDisabled = button.context.getColorFromAttr(android.R.attr.colorBackground)
 
@@ -580,7 +580,7 @@ class AppDetailAdapter(private val callbacks: Callbacks) :
                 with(title) {
                     gravity = Gravity.CENTER
                     typeface = TypefaceExtra.medium
-                    setTextColor(context.getColorFromAttr(MaterialR.attr.colorPrimaryContainer))
+                    setTextColor(context.getColorFromAttr(MaterialR.attr.colorPrimary))
                     setTextSizeScaled(20)
                     setText(stringRes.application_not_found)
                     setPadding(0, 12.dp, 0, 12.dp)
@@ -619,7 +619,7 @@ class AppDetailAdapter(private val callbacks: Callbacks) :
                 with(repoTitle) {
                     gravity = Gravity.CENTER
                     typeface = TypefaceExtra.medium
-                    setTextColor(context.getColorFromAttr(MaterialR.attr.colorPrimaryContainer))
+                    setTextColor(context.getColorFromAttr(MaterialR.attr.colorPrimary))
                     setTextSizeScaled(20)
                     setPadding(0, 0, 0, 12.dp)
                 }
@@ -635,8 +635,8 @@ class AppDetailAdapter(private val callbacks: Callbacks) :
                     icon = context.open
                     setText(stringRes.add_repository)
                     setBackgroundColor(context.getColor(android.R.color.transparent))
-                    setTextColor(context.getColorFromAttr(MaterialR.attr.colorPrimaryContainer))
-                    iconTint = context.getColorFromAttr(MaterialR.attr.colorPrimaryContainer)
+                    setTextColor(context.getColorFromAttr(MaterialR.attr.colorPrimary))
+                    iconTint = context.getColorFromAttr(MaterialR.attr.colorPrimary)
                 }
                 addView(
                     title,
