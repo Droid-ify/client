783ac8e8f328624e1a09db069eadcd038e400a3c
LooKeR <iamlooker@proton.me>
2026-01-03 12:38:26 +0530

    build: Wrap repository description in `Html` value class

--- app/src/androidTest/kotlin/com/looker/droidify/sync/common/Repo.kt
+++ app/src/androidTest/kotlin/com/looker/droidify/sync/common/Repo.kt
@@ -2,6 +2,7 @@ package com.looker.droidify.sync.common
 
 import com.looker.droidify.data.model.Authentication
 import com.looker.droidify.data.model.Fingerprint
+import com.looker.droidify.data.model.Html
 import com.looker.droidify.data.model.Repo
 import com.looker.droidify.data.model.VersionInfo
 
@@ -10,7 +11,7 @@ val Izzy = Repo(
     enabled = true,
     address = "https://apt.izzysoft.de/fdroid/repo",
     name = "IzzyOnDroid F-Droid Repo",
-    description = "This is a repository of apps to be used with F-Droid. Applications in this repository are official binaries built by the original application developers, taken from their resp. repositories (mostly Github, GitLab, Codeberg). Updates for the apps are usually fetched daily, and you can expect daily index updates.",
+    description = Html("This is a repository of apps to be used with F-Droid. Applications in this repository are official binaries built by the original application developers, taken from their resp. repositories (mostly Github, GitLab, Codeberg). Updates for the apps are usually fetched daily, and you can expect daily index updates."),
     fingerprint = Fingerprint("3BF0D6ABFEAE2F401707B6D966BE743BF0EEE49C2561B9BA39073711F628937A"),
     authentication = Authentication("", ""),
     versionInfo = VersionInfo(0L, null),
