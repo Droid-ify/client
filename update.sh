fdb60b0ef26b6efb24c6f95d4b9c0e4b9c2b14d4
LooKeR <iamlooker@proton.me>
2026-02-23 01:09:40 +0530

    fix: Update script
    Signed-off-by: LooKeR <iamlooker@proton.me>

--- update.sh
+++ update.sh
@@ -56,6 +56,7 @@ else
 fi
 
 # Update the Kotlin file with new version code and name
+sed -i "s/versionCode = [0-9]*/versionCode = $version_code/" "$kotlin_file"
 sed -i "s/val latestVersionName = \"[^\"]*\"/val latestVersionName = \"$version_name\"/" "$kotlin_file"
 
 # Create a changelog file
