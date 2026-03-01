e011102832bb7422385c33f8ff67035ef01e7b36
LooKeR <iamlooker@proton.me>
2026-03-01 18:10:29 +0530

    fix(ui): Settings dialog closing on state change
    Closes #1243
    
    Signed-off-by: LooKeR <iamlooker@proton.me>

--- app/src/main/kotlin/com/looker/droidify/compose/settings/components/SelectionSettingItem.kt
+++ app/src/main/kotlin/com/looker/droidify/compose/settings/components/SelectionSettingItem.kt
@@ -14,7 +14,7 @@ import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -35,7 +35,7 @@ fun <T> SelectionSettingItem(
     dialogIcon: ImageVector? = null,
     enabled: Boolean = true,
 ) {
-    var showDialog by remember { mutableStateOf(false) }
+    var showDialog by rememberSaveable { mutableStateOf(false) }
 
     Column(
         modifier = modifier
