aa624d0434378d85e0d4b334ec02fd922b9d6da0
A-man404 <amansaini04a@gmail.com>
2026-06-23 15:39:58 +0530

    Revert "made a db call in the ui thread"
    This reverts commit 8029cc3625bb2280bad50c5e67a7845b3dad4b25.

--- app/src/main/kotlin/com/looker/droidify/Droidify.kt
+++ app/src/main/kotlin/com/looker/droidify/Droidify.kt
@@ -47,6 +47,7 @@ import io.ktor.client.HttpClient
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.SupervisorJob
+import kotlinx.coroutines.cancel
 import kotlinx.coroutines.flow.collectIndexed
 import kotlinx.coroutines.flow.drop
 import kotlinx.coroutines.launch
@@ -56,7 +57,6 @@ import kotlin.time.Duration.Companion.hours
 
 @HiltAndroidApp
 class Droidify : Application(), SingletonImageLoader.Factory, Configuration.Provider {
-    private val applicationScope = CoroutineScope(SupervisorJob() + Dispatchers.IO)
 
     private val parentJob = SupervisorJob()
     private val appScope = CoroutineScope(Dispatchers.Default + parentJob)
@@ -87,11 +87,7 @@ class Droidify : Application(), SingletonImageLoader.Factory, Configuration.Prov
         updatePreference()
         appScope.launch { installer() }
 
-        if (databaseUpdated) {
-            applicationScope.launch {
-                forceSyncAll()
-            }
-        }
+        if (databaseUpdated) forceSyncAll()
     }
 
     private fun listenApplications() {
