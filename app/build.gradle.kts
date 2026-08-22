39c045632965f2ae499aa0bd11ce70278064f30e
LooKeR <iamlooker@proton.me>
2026-08-22 12:16:53 +0530

    Release 0.7.6

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,7 +10,7 @@ plugins {
 }
 
 android {
-    val latestVersionName = "0.7.5"
+    val latestVersionName = "0.7.6"
     namespace = "com.looker.droidify"
     compileSdk {
         version = release(36)
@@ -20,7 +20,7 @@ android {
         applicationId = "com.looker.droidify"
         minSdk = 23
         versionName = latestVersionName
-        versionCode = 750
+        versionCode = 760
 
         testInstrumentationRunner = "com.looker.droidify.TestRunner"
     }
